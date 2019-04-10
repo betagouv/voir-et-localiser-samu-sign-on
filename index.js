ce7e1fe1b38a56c897901a1ee964ae07b3c6fdb0
Thomas Guillet <guillet.thomas@gmail.com>
2019-04-10 07:35:53 +0200

    Ajoute /

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var app = express();
+var mustache = require('consolidate').mustache
+var express = require('express')
+var app = express()
 
-var port = 10101;
+var port = 10101
+
+app.engine('html', mustache)
+app.set('view engine', 'html')
 app.listen(port, function () {
-    console.log('Server listening on port %d', port);
-});
+    console.log('Server listening on port %d', port)
+})
+
+app.get("/", function(req, res) {
+  res.render('index')
+})
