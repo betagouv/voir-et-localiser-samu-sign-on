3a705eb7db7e44459dffdf8b83f443ea1227c043
Romy Alula <goumiesone.dev@gmail.com>
2019-04-11 19:00:18 +0200

    Add redirect_uri passed in request
    Create valid user in db
    Find user in db with middleware getToken

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.post('/', validateUser, (req, res) => {
   if (req.query.redirect_uri) {
-    return res.redirect(req.query.redirect_uri);
+    return res.redirect(`${req.query.redirect_uri}?code=${req.user.id}`);
   }
   res.json({ login: 'ok', userId: req.user.id });
 });
@@ -41,10 +41,39 @@ app.post('/users/new', (req, res) => {
     password: req.body.password,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-  }).then(u => {
+    role: req.body.role,
+    unit: req.body.unit,
+    department: req.body.department,
+  }).then((u) => {
     res.redirect('/users');
-  }).catch(e => {
+  }).catch((e) => {
     console.log(e);
     res.render('users/new', { error: e });
+  });
+});
+
+function getToken(req, res, next) {
+  const header = req.get('Authorization');
+  if (!header/* missing token */) {
+    return res.json({ error: 'missing token' });
+  }
+
+  const code = parseInt(header.replace('Bearer', ''), 10);
+  if (code.isNaN) {
+    return res.json({ error: 'unvalid token' });
+  }
+
+  req.token = code;
+  return next();
+}
+
+app.get('/api/userinfo', getToken, (req, res) => {
+  User.findOne({
+    where: {
+      id: req.token,
+    },
+    attributes: { exclude: ['password'] },
+  }).then((user) => {
+    res.json(user);
   });
 });
