a573fb2ff9983339cac47251ec747bb4fda6df26
Romy Alula <goumiesone.dev@gmail.com>
2019-05-29 11:28:37 +0200

    Mise à jour de la configuration et du script de migration

--- src/migrations/toV2.sql
+++ src/migrations/toV2.sql
@@ -1,2 +1,13 @@
+create table email_tokens
+(
+    id        CHAR(40) not null
+        primary key
+        unique,
+    createdAt DATETIME not null,
+    userId    INTEGER  not null
+        references users
+            on update cascade on delete cascade
+);
+
 alter table users
 	add emailConfirmationTokenAt datetime default null;
